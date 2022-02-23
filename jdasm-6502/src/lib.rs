e63f566bcf981221d873ad965512260da7763fbb
osamaar <osama.arafa@outlook.com>
2022-02-22 21:55:32 -0500

    Implement disasm for Read data sources

--- jdasm-6502/src/lib.rs
+++ jdasm-6502/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, io::{Read, Bytes}};
 
 use crate::constant::OPTABLE;
 
@@ -88,17 +88,17 @@ pub enum Mnemonic {
 /// end or when next bytes don't make sense. Therefore, it doesn't guarantee
 /// that all of `data` is read. To count bytes read, add `.offset` + `.size` of
 /// last instruction in returned `Vec`.
-pub fn disasm_all(data: &[u8]) -> Vec<Instruction> {
+pub fn disasm_all<R: Read>(data: &mut R) -> Vec<Instruction> {
     disasm(data).collect()
 }
 
-pub struct AsmIter<'a> {
-    data: &'a [u8],
+pub struct AsmIter<'a, R: Read> {
+    data: &'a mut R,
     offset: usize,
 }
 
-impl<'a> AsmIter<'a> {
-    fn new(data: &'a [u8]) -> Self {
+impl<'a, R: Read> AsmIter<'a, R> {
+    fn new(data: &'a mut R) -> Self {
         AsmIter { data, offset: 0 }
     }
 
@@ -107,14 +107,13 @@ impl<'a> AsmIter<'a> {
     }
 }
 
-impl Iterator for AsmIter<'_> {
+impl<R: Read> Iterator for AsmIter<'_, R> {
     type Item = Instruction;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Ok(mut instr) = disasm_one(self.data) {
+        if let Ok(mut instr) = disasm_one(&mut self.data) {
             instr.offset = self.offset;
             self.offset += instr.op.size as usize;
-            self.data = &self.data[instr.op.size as usize..];
             Some(instr)
         } else {
             None
@@ -122,35 +121,48 @@ impl Iterator for AsmIter<'_> {
     }
 }
 
+/// Helper function to read one byte at a time.
+fn read_byte<R: Read>(bytes: &mut Bytes<R>) -> Result<u8, ()> {
+    match bytes.next() {
+        Some(r) => r.or_else(|_| Err(())),
+        None => Err(()),
+    }
+}
+
 /// Return an itarator over disassembled instructinos from `data`.
-pub fn disasm(data: &[u8]) -> AsmIter {
+pub fn disasm<R: Read>(data: &mut R) -> AsmIter<R> {
     AsmIter::new(data)
 }
 
 /// Disassemble one instructino from data slice.
-pub fn disasm_one(data: &[u8]) -> Result<Instruction, ()> {
-    if data.len() == 0 {
-        return Err(());
-    }
-
-    let op = OPTABLE[data[0] as usize];
+pub fn disasm_one<R: Read>(data: &mut R) -> Result<Instruction, ()> {
+    let mut bytes = data.bytes();
+    let op = OPTABLE[read_byte(&mut bytes)? as usize];
     
     match op.size {
-        n if n as usize > data.len() => Err(()),
+        // n if n as usize > data.len() => Err(()),
         1 => Ok(Instruction{ op, operand: Operand::Null, offset: 0 }),
-        2 => Ok(Instruction{ op, operand: Operand::Byte(data[1]), offset: 0}),
+
+        2 => {
+            let operand = Operand::Byte(read_byte(&mut bytes)?);
+            Ok(Instruction{ op, operand, offset: 0 })
+        },
+
         3 => {
-            let op0 = data[1] as u16;
-            let op1 = data[2] as u16;
+            let op0 = read_byte(&mut bytes)? as u16;
+            let op1 = read_byte(&mut bytes)? as u16;
             let operand = Operand::Word(op0 | (op1 << 8));
             Ok(Instruction{ op, operand, offset: 0})
         },
+
         _ => unreachable!()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::io::Cursor;
+
     use super::*;
 
     fn hex_to_bin(s: &str) -> Vec<u8> {
@@ -162,7 +174,7 @@ mod tests {
     #[test]
     fn disasm_one_ok() {
         let prog = hex_to_bin("a9 01 8d 00 02 a9 05 8d 01 02 a9 08 8d 02 02");
-        let instr = disasm_one(&prog);
+        let instr = disasm_one(&mut Cursor::new(prog));
         let s = format!("{}", instr.unwrap());
 
         assert_eq!(s, "0000: LDA #$01");
@@ -171,7 +183,8 @@ mod tests {
     #[test]
     fn disasm_ok() {
         let prog = hex_to_bin("a9 01 8d 00 02 a9 05 8d 01 02 a9 08 8d 02 02");
-        let mut iter = disasm(&prog);
+        let mut cursor = Cursor::new(prog);
+        let mut iter = disasm(&mut cursor);
         let f = |i| format!("{}", i);
 
         assert_eq!(iter.next().map(f), Some("0000: LDA #$01".to_owned()));
@@ -186,7 +199,7 @@ mod tests {
     #[test]
     fn disasm_all_ok() {
         let prog = hex_to_bin("a9 01 8d 00 02 a9 05 8d 01 02 a9 08 8d 02 02");
-        let instr  = disasm_all(&prog);
+        let instr  = disasm_all(&mut Cursor::new(prog));
         let lines: Vec<_> = instr.iter().map(|ln| format!("{}", ln)).collect();
 
         assert_eq!(lines[0], "0000: LDA #$01");
